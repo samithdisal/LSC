25e6d954ff38c3ed9cfa07aafae61ae7991d48e6
Samith Sandanayake <sam.the.devel@gmail.com>
2015-08-01 22:17:17 +0530

    added a timeout parameter to and connection re-try to fix connection fail issues

--- src/main/scala/app/LSC.scala
+++ src/main/scala/app/LSC.scala
@@ -22,6 +22,8 @@ object LSC extends App {
 
   private var verbose: Boolean = false
 
+  private var interval: Long = 2000
+
   private def log(msg: String): Unit = {
     if(verbose) {
       println(msg)
@@ -40,7 +42,10 @@ object LSC extends App {
     }
   }
 
+  var errorCount = 0
+
   private def download(link: String): Unit = {
+    Thread.sleep(interval)
     try {
       println(s"Grabbing story $link")
       val doc: Document = Jsoup.connect(link).get()
@@ -48,7 +53,7 @@ object LSC extends App {
       log(s"Title: $title")
       val author = doc.select("span.b-story-user-y.x-r22 a").text()
       log(s"Author: $author")
-      val name = s"$outputDir${if (!outputDir.endsWith("/")) "/"}" +
+      val name = s"$outputDir${if (!outputDir.endsWith("/")) "/" else ""}" +
         (author + "_" + title).replaceAll("\\s", "_") + ".txt"
 
       log(s"File: $name")
@@ -60,10 +65,19 @@ object LSC extends App {
       writer.write(s"\n\n############\nFetched on ${new Date().toString}")
       writer.flush()
       writer.close()
+      errorCount = 0
     } catch {
       case e: IOException =>
-        System.exit(1)
-
+        println(s"Cannot read $link")
+        if (errorCount < 3) {
+          Thread.sleep(3000)
+          errorCount += 1
+          println(s"Retrying $link")
+          download(link)
+        } else {
+          println("Error persists, exiting")
+          System.exit(1)
+        }
     }
   }
 
@@ -77,6 +91,7 @@ object LSC extends App {
       urls.foreach{x=>download(x.attr("href"))}
     } catch {
       case e: IOException =>
+        println("Cannot read author's submissions page")
         System.exit(1)
     }
   }
@@ -89,6 +104,7 @@ object LSC extends App {
   cliOptions.addOption("p", "port", true, "port to connect to")
   cliOptions.addOption("o", "out", true, "output directory")
   cliOptions.addOption("v", "verbose", false, "make verbose")
+  cliOptions.addOption("i", "interval", true, "interval between each fetch in seconds")
 
   //Main
   try {
@@ -108,6 +124,14 @@ object LSC extends App {
       verbose = true
     }
 
+    if(parsed.hasOption("i")) {
+      try {
+        interval = Option(parsed.getOptionValue("i")).map(_.toLong * 1000).getOrElse(2000)
+      } catch {
+        case e: Exception => println("Invalid interval value, skipping option")
+      }
+    }
+
     if (parsed.hasOption("a")) {
       readAllAuthor(parsed.getOptionValue("a"))
       println("Downloaded author")
@@ -115,10 +139,10 @@ object LSC extends App {
       download(parsed.getOptionValue("s"))
       println("Downloaded story")
     } else {
-      formatter.printHelp( "LSC", cliOptions );
+      formatter.printHelp( "LSC", cliOptions )
     }
   } catch {
     case e: IndexOutOfBoundsException =>
-      formatter.printHelp( "LSC", cliOptions );
+      formatter.printHelp( "LSC", cliOptions )
   }
 }
